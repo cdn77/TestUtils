fc7850146d4e847a351121bb2f96c50cb50a6280
Simon Podlipsky <simon@podlipsky.net>
2023-03-13 13:23:07 +0100

    refactor: drop deprecated EveryTestHasSameNamespaceAsTestedClass check (#72)

--- tests/TestCheck/Fixtures/EveryTestHasSameNamespaceAsTestedClass/SameNamespace.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Cdn77\TestUtils\Tests\TestCheck\Fixtures\EveryTestHasSameNamespaceAsTestedClass;
-
-final class SameNamespace
-{
-}
--- tests/TestCheck/Fixtures/EveryTestHasSameNamespaceAsTestedClass/tests/MissingAnnotationsTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Cdn77\TestUtils\Tests\Tests\TestCheck\Fixtures\EveryTestHasSameNamespaceAsTestedClass;
-
-final class MissingAnnotationsTest
-{
-}
